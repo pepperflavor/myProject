c87bb98d089e130d26fd0288b4c8669da38f3019
pepperflavor <pepperflavor@gmail.com>
2022-10-13 21:46:08 +0900

    react-native 연습

--- reactstudy/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ reactstudy/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -3,36 +3,42 @@ import React, { useState } from "react";
 import "./NewExpense.css"
 import ExpenseForm from "./ExpenseForm";
 
+
+// 강의 정답카피
 const NewExpense = (props)=>{
+
     const saveExpenseDataHandler = (enterdExpenseData) => {
         const expenseData = {
             ...enterdExpenseData,
             id : Math.random().toString(),
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false)
         console.log(expenseData);
     }
 
     // 입력창 보일지 말지 조건설정
-    const [isClicked, setIsClicked] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
 
-    const addSubmitHandler = (event) => {
-      event.preventDefault();
-      setIsClicked(true);
-      console.log(isClicked);
-    };
+
+    const startEditingHandler = () =>{
+      setIsEditing(true)
+    }
+
+    const stopEditingHandler = () =>{
+      setIsEditing(false);
+    }
 
     return (
       <div className="new-expense">
         {/* 조건 따라서 보일지말지 */}
-        {!isClicked && (
-          <button onClick={addSubmitHandler}>Add New Expense</button>
+        {!isEditing && (
+          <button onClick={startEditingHandler}>Add New Expense</button>
         )}
-        {isClicked && (
+        {isEditing && (
           <ExpenseForm
             onSaveExpenseData={saveExpenseDataHandler}
-            onSaveIsClicked={setIsClicked}
-            formSelected={isClicked}
+            onCancel={stopEditingHandler}
           />
         )}
       </div>
