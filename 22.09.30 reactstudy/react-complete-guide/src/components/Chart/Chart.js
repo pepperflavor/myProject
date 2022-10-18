fbcbe767e5d1f7bfe224f34c6fa668900dcb00e4
unknown <j8747j@gmail.com>
2022-10-19 05:23:47 +0900

    차트 수작업업데이트

--- 22.09.30 reactstudy/react-complete-guide/src/components/Chart/Chart.js
+++ 22.09.30 reactstudy/react-complete-guide/src/components/Chart/Chart.js
@@ -4,9 +4,13 @@ import './Chart.css'
 import ChartBar from './ChartBar'
 
 const Chart = (props) => {
-  let barFillHeight = '0%';
-
-  if(props.max > 0)
+  // 차트에 표시할 최대 값을 구해야함
+  // 특정 값들만 추출 & 숫자로 변환
+  const dataPointValue = props.dataPoints.map(dataPoint => dataPoint.value); // 배열임
+  
+  // max() 인자로 숫자를 넣어야함, 인자중 가장 큰 값을 반환함
+  // 숫자를 인자로 넣어야하기때문에 Math.max(...dataPointValue);  스프레드 연산자로 12개의 인수를 넣음
+  const totalMaximum = Math.max(...dataPointValue); 
 
   return (  
     <div className="chart">
@@ -15,7 +19,7 @@ const Chart = (props) => {
           <ChartBar
               key={dataPoint.label}
               value={dataPoint.value}
-              maxVlaue={""}
+              maxVlaue={totalMaximum}
               label={dataPoint.label}
           />
       ))}
