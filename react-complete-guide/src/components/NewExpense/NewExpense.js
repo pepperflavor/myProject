d0d55e1e6892406c36b4c07ad6f5394feb725680
pepperflavor <pepperflavor@gmail.com>
2022-11-07 17:51:54 +0900

    useState()로 상태관리

---
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './NewExpense.css'
+
+const NewExpense =() =>{
+    return (
+      <div className="new-expense">
+        <div></div>
+      </div>
+    );
+}
+
+export default NewExpense;
